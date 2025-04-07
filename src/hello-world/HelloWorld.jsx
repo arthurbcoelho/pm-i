a15064847ce4b69164ddb5a740aba136ff611a3b
Arthur Coelho <abcoelhojc@gmail.com>
2025-04-07 19:48:51 -0300

    first commit

---
+++ src/hello-world/HelloWorld.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+const HelloWorld = () => {
+
+    let [name, setName] = useState('');
+    let [message, setMessage] = useState('');
+
+    const handleHelloWorld = (event) => {
+        setMessage(`Hello world, ${name}`)
+    };
+
+    function handleClear() {
+        setName('');
+        setMessage('');
+    };
+
+    const handleChangeInput = (event) => {
+        setName(event.target.value);
+        console.log('name: ', name);
+    }
+
+    const handleKeyInput = (event) => {
+        console.log(event);
+        if (event.key === 'Escape') {
+            handleClear();
+        } else if(event.key === 'Enter') {
+            handleHelloWorld();
+        }
+    };
+
+    return (
+        <>
+            <h1>Hello World!</h1>
+            <input type="text" onChange={handleChangeInput} onKeyUp={handleKeyInput} value={name} />
+            <button onClick={handleHelloWorld}>Hello World</button>
+            <button onClick={handleClear}>Limpar</button>
+            {message && <p>{message}</p>}
+        </>
+    )
+}
+
+export default HelloWorld;
+
