1c45d85f152998ef32030df711d7f211cd86f008
Arthur Coelho <abcoelhojc@gmail.com>
2025-04-07 21:00:35 -0300

    add calculadoras

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
 import HelloWorld from './hello-world/HelloWorld'
+import Calculadora from './calculadora/Calculadora'
+import CalculadoraIMC from './calculadora-imc/CalculadoraIMC'
 
 function App() {
   return (
-    <HelloWorld />
+    // <HelloWorld />
+    <CalculadoraIMC />
   );
 }
 
